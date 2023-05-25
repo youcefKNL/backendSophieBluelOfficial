a0163db9e234bed675ee9d901494750ef58da46a
youcefKNL <khenichil_youcef@hotmail.fr>
2023-05-25 23:50:34 +0200

    first

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(
-// helmet({
-//   crossOriginResourcePolicy: false,
-// })
-//);
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 const db = require("./models");
